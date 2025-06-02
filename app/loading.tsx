0a1d3583dcc5c89740ac68cb2bf7d7b3b36e0f87
Krish Shaw <shawkrish418@gmail.com>
2025-06-02 12:28:06 +0530

    Loader in Next.js

---
+++ app/loading.tsx
@@ -0,0 +1,5 @@
+export default function Loader(){
+    return (
+        <div className="flex justify-center items-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">Loading...</div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,29 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-export default function Home() {
+import axios from "axios";
+async function getUserDetails(){
+  const response = await axios.get(`https://jsonplaceholder.typicode.com/users/1`);
+  if(response)
+    return response.data;
+  else return "Error fetching data!"
+}
+export default async function Home() {
+  const userData =await getUserDetails();
   return (
-      <div className="border-b p-4 font-bold flex justify-between items-center">
+    <div className="flex flex-col justify-center gap-56">
+      <div className="border-b p-4 font-bold flex justify-between items-center relative">
           <h1>Acme Inc.</h1>
           <div>
             <Button variant="ghost"><Link href="/signup">Signup</Link></Button>
             <Button variant="ghost"><Link href="/login">Login</Link></Button>
           </div>
       </div>
+      <div className="border flex flex-col justify-center w-60 h-35 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 absolute">
+          <span className="flex justify-center">Id: {userData.id}</span>
+          <span className="flex justify-center">Name: {userData.name}</span>
+          <span className="flex justify-center">Username: {userData.username}</span>
+          <span className="flex justify-center">Email: {userData.email}</span>
+      </div>
+    </div>
   );
 }
