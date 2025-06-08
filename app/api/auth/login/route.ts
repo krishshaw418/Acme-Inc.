d37d8601169b32f616414b6e65b9671f686efcfc
Krish Shaw <shawkrish418@gmail.com>
2025-06-09 01:18:03 +0530

    added cookies to login route

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 import { prisma }from "@/lib/prisma";
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
 
 export async function POST(req: Request){
     const {email, password} = await req.json();
@@ -8,8 +10,17 @@ export async function POST(req: Request){
         const user = await prisma.user.findUnique({
             where: { email }
         });
+        if(!user) return NextResponse.json({message:"User not found please signup!"});
         if(user?.password !== password) return NextResponse.json({message: "Invalid Credentials!"});
-        if(!user) return NextResponse.json({messahe:"User not found please signup!"});
+        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET!, {expiresIn : '1h'});
+        if(!token) return NextResponse.json({message: "Error generating token!"});
+        (await cookies()).set("access-token", token, {
+            httpOnly: true,
+            secure: true,
+            maxAge: 60*60,
+            path: "/",
+            sameSite: "none"
+        })
         return NextResponse.json({message: "Login Sucessful!"});
     } catch (error) {
         return NextResponse.json({message: "Server error please try again!"});
--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -22,7 +22,8 @@ export async function POST(req: Request){
           httpOnly: true,
           secure: process.env.NODE_ENV === "production",
           maxAge: 60*60,
-          path: "/"
+          path: "/",
+          sameSite: "none"
         });
         return NextResponse.json({message: "Signup Successful!"});
     } catch (error) {
