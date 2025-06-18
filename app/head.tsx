c9f6d91a15983e11339f1567cb289f8829aa8806
Krish Shaw <shawkrish418@gmail.com>
2025-06-18 13:39:07 +0530

    Indexing on Google search console

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Acme Inc.</title>
+      <meta name="description" content="Residential Properties at Reasonable Price" />
+      <meta name="google-site-verification" content="9O0vJULMwTC08hObJDQr3fh19KKknI9X0_n5ukIHO8I" />
+    </>
+  );
+}
