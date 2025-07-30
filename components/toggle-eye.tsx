3b963dfebcd9a6eb2a13a5beda8025af137c93d0
Krish Shaw <shawkrish418@gmail.com>
2025-07-30 21:54:08 +0530

    fix(Linting error): fix Error: Unexpected any

--- components/toggle-eye.tsx
+++ components/toggle-eye.tsx
@@ -1,6 +1,12 @@
 import { Eye, EyeClosed } from "lucide-react"
+import React from "react";
 
-export default function Toggle(props: any){
+type View = {
+    view: boolean;
+    setView: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function Toggle(props: View){
     
     return props.view ? <Eye onClick={() => props.setView(false)} className="text-gray-400"/> : <EyeClosed onClick={() => props.setView(true)} className="text-gray-400"/>
     
